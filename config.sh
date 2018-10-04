e90bc10bf44f72d2ab269453e4da95e5dc2da1a8
Victor Utreras <vutreras@live.com>
2018-10-04 18:00:26 -0300

    Changed the method for get the sdk php

--- config.sh
+++ config.sh
@@ -3,19 +3,17 @@
 #Script for configure the plugin project
 
 PHP_SDK_VERSION='1.3.2'
-REPO_SDK='https://github.com/TransbankDevelopers/transbank-sdk-php.git'
+REPO_SDK="https://github.com/TransbankDevelopers/transbank-sdk-php/archive/$PHP_SDK_VERSION.zip"
 DIR_DEST_SDK='upload/system/library/transbank-sdk-php'
 
 echo "Removing the older SDK"
-rm -rf upload/system/library/transbank-sdk-php
+rm -rf "$DIR_DEST_SDK**"
 
-echo "Cloning SDK version: $PHP_SDK_VERSION"
-git clone https://github.com/TransbankDevelopers/transbank-sdk-php.git $DIR_DEST_SDK
+echo "Downloading SDK version: $PHP_SDK_VERSION"
+wget $REPO_SDK -O "$PHP_SDK_VERSION.zip"
+unzip "$PHP_SDK_VERSION.zip" -d upload/system/library
+rm -rf "$PHP_SDK_VERSION.zip"
+mv "$DIR_DEST_SDK-$PHP_SDK_VERSION" $DIR_DEST_SDK
 
 echo "Changing to SDK version: $PHP_SDK_VERSION"
-cd $DIR_DEST_SDK
-git fetch && git fetch --tags
-git checkout $PHP_SDK_VERSION
-git status
 echo "SDK version: $PHP_SDK_VERSION"
-cd ../../../../
