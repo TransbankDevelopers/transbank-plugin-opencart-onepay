321540436c870fed6b9b4304f635f34b29230dc8
Victor Utreras <vutreras@live.com>
2018-10-05 10:57:16 -0300

    Moven sources to dir src

--- config.sh
+++ config.sh
@@ -4,14 +4,14 @@
 
 PHP_SDK_VERSION='1.3.2'
 REPO_SDK="https://github.com/TransbankDevelopers/transbank-sdk-php/archive/$PHP_SDK_VERSION.zip"
-DIR_DEST_SDK='upload/system/library/transbank-sdk-php'
+DIR_DEST_SDK='src/upload/system/library/transbank-sdk-php'
 
 echo "Removing the older SDK"
 rm -rf "$DIR_DEST_SDK**"
 
 echo "Downloading SDK version: $PHP_SDK_VERSION"
 wget $REPO_SDK -O "$PHP_SDK_VERSION.zip"
-unzip "$PHP_SDK_VERSION.zip" -d upload/system/library
+unzip "$PHP_SDK_VERSION.zip" -d src/upload/system/library
 rm -rf "$PHP_SDK_VERSION.zip"
 mv "$DIR_DEST_SDK-$PHP_SDK_VERSION" $DIR_DEST_SDK
 
--- package.sh
+++ package.sh
@@ -8,12 +8,18 @@ if [[ ! -v TRAVIS_TAG ]]; then
     TRAVIS_TAG='1.0.0'
 fi
 
-sed -i "s/PLUGIN_VERSION = '1.0.0';/PLUGIN_VERSION = '${TRAVIS_TAG}';/g" upload/system/library/TransbankSdkOnepay.php
-sed -i "s/<version>1.0.0/<version>${TRAVIS_TAG}/g" install.xml
+sed -i "s/PLUGIN_VERSION = '1.0.0';/PLUGIN_VERSION = '${TRAVIS_TAG}';/g" src/upload/system/library/TransbankSdkOnepay.php
+sed -i "s/<version>1.0.0/<version>${TRAVIS_TAG}/g" src/install.xml
 
 PLUGIN_FILE="plugin-transbank-onepay-opencart3-$TRAVIS_TAG.ocmod.zip"
 
-zip -FSr $PLUGIN_FILE . -x docs/\* *.git/\* .DS_Store* .editorconfig* .gitignore* .vscode/\* *.sh .travis* README.md *.zip docker-opencart3/\*
+cp CHANGELOG.md src/
+cp LICENSE src/
+cd src
+zip -FSr ../$PLUGIN_FILE . -x *.git/\* .DS_Store* *.zip
+cd ..
+rm src/CHANGELOG.md
+rm src/LICENSE
 
 echo "Plugin version: $TRAVIS_TAG"
 echo "Plugin file: $PLUGIN_FILE"
